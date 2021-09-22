230bf5f006fb6f196aa28878add7811fe31af3bd
unknown <nghiazexal@gmail.com>
2021-09-22 08:15:51 +0700

    first commit

---
+++ src/main/java/com/example/spring_boot_jwt_tmn/SpringBootJwtTmnApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_tmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtTmnApplication.class, args);
+    }
+
+}
